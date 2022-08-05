26cbcee20686e78667f6a73ee00a245e01d5165a
Juneha <junehahwang@gmail.com>
2022-08-05 16:24:32 +0900

    hackathon

--- emission_calculator.py
+++ emission_calculator.py
@@ -1,39 +1,46 @@
+import matplotlib.pyplot as plt
+
+
 def calculate_emissions():
     emissions = 0
 
-    # transportation
-    type_of_vehicle = int(input("What type of vehicle do you use? enter 1 for car: "))
-    fuel_economy = 10.6286  # km/liter
-    distance = int(input("How long do you drive in km?: "))
-    gas_per_fuel = 2.3
-    emissions += fuel_economy * distance * gas_per_fuel
+    car = int(input("Do you drive a car? (1-yes, 0-no): "))
+    if car:
+        fuel_economy = 10.6286  # km/liter
+        distance = float(input("How long do you drive in km?: "))
+        gas_per_fuel = 2.3
+        emissions += distance/fuel_economy * gas_per_fuel
 
     # Home Energy
-    natural_gas = int(input("How much gas does your home use? (kg): ")) * 1.9
-    electricity = int(input("How much electricity does your home use? (kWh): ")) * 0.709
+    natural_gas = float(input("How much gas does your home use? (kg): ")) * 1.9
+    electricity = float(input("How much electricity does your home use? (kWh): ")) * 0.709
     emissions += natural_gas + electricity
 
     # Waste
-    cans = int(input("Roughly how many aluminum cans do you throw away?: ")) * 0.155
-    plastic = int(input("Roughly how much plastic do you throw away? (in kg): ")) * 6.02
-    glass = int(input("Roughly how much glass do you throw away (in kg): ")) * 8.4
-    paper = int(input("Roughly how much paper do you throw away? (in kg): ")) * 0.028
+    cans = float(input("Roughly how many aluminum cans do you throw away?: ")) * 0.155
+    plastic = float(input("Roughly how much plastic do you throw away? (in kg): ")) * 6.02
+    glass = float(input("Roughly how much glass do you throw away (in kg): ")) * 8.4
+    paper = float(input("Roughly how much paper do you throw away? (in kg): ")) * 0.028
     emissions += cans + plastic + glass + paper
-
-    emissions = int(emissions)
 
     print("Your total greenhouse gas emissions:", emissions, "kg")
     return emissions
 
-record = []
 
-import matplotlib.pyplot as plt
-x = ["Monday", "Tuesday", "Wednesday"]
-y1 = [123, 141, 103]
-y2 = [43, 54, 41]
-plt.plot(x, y1, 'r-')
-plt.plot(x, y2, 'b-')
+num_days = int(input("For how many days would you like this program to calculate your greenhouse gas emissions?: "))
+record = []
+for i in range(num_days):
+    print("DAY", i+1, "----------------------------------------------------")
+    result = calculate_emissions()
+    record.append(result)
+
+
+x = list(i for i in range(1, num_days+1))
+average = [26.57]*num_days
+plt.plot(x, record, 'r-', label="Your Emissions")
+plt.plot(x, average, 'b-', label="Japan's Average")
 plt.xlabel("Days")
-plt.ylabel("Greenhouse gas emissions")
-plt.title('Your Greenhouse Gas Emissions This Week!')
+plt.ylabel("Greenhouse gas emissions (kg)")
+plt.title('Your Greenhouse Gas Emissions')
+plt.legend()
 plt.show()
